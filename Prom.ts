d33ea71169ee5eeb89379b176de5b85e264b835b
ShaunPark <coolage73@gmail.com>
2021-08-03 14:36:11 +0900

    a

--- Prom.ts
+++ Prom.ts
@@ -11,10 +11,25 @@ class PromTest {
 
     run = async () => {
         const q = 'node_memory_Cached_bytes{instance="10.0.0.8:9100", job="server-info"}'
-        const {result, resultType} = await this.prom.instantQuery(q)
+        const { result, resultType } = await this.prom.instantQuery(q)
         console.log(JSON.stringify(result))
         console.log(JSON.stringify(resultType))
+
+    }
+
+    run2 = async () => {
+        const q = 'node_memory_Cached_bytes{instance="10.0.0.8:9100", job="server-info"}'
+
+        const now = Date.now()
+        const start = now - 60 * 60 * 1000
+        const end = now
+        const step = 60
+        const { result } = await this.prom.rangeQuery(q, start, end, step)
+
+        result.forEach(r => {
+            console.log(JSON.stringify(r))
+        })
     }
 }
 
-new PromTest().run()
+new PromTest().run2()
